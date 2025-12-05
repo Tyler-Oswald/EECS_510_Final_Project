020aae976c69e97ac7c9bfce0acb0ce8d1db6f5b
HairyTurtle00 <hairyturtle00@gmail.com>
2025-12-05 16:28:22 -0600

    Added comment

--- Simulator/sim.py
+++ Simulator/sim.py
@@ -1,22 +1,28 @@
 from turing import state, Turing
-
+#Function to convert the file data structure
 def load_tm(filename):
+    #Hold information about the machine 
     transitions = {}
     start_state = None
     accepting_state = None
     states_list = []
-
+    #Open file
     with open(filename, "r") as f:
+        #Parse each line in the file 
         for line in f:
             line = line.strip()
             if not line:
                 continue
+            #Case for the states 
             if line.startswith("States:"):
                 states_list = line.split(":", 1)[1].strip().split()
+            #Case for the start states 
             elif line.startswith("Start_state:"):
                 start_state = line.split(":", 1)[1].strip()
+            #Case for the accepting states 
             elif line.startswith("Accepting_State:"):
                 accepting_state = line.split(":", 1)[1].strip()
+            #Case for the transitions 
             elif line.startswith("Transitions:"):
                 line = line.split(":", 1)[1].strip()
                 trans_strings = line.split()
@@ -30,35 +36,46 @@ def load_tm(filename):
                     if src not in transitions:
                         transitions[src] = []
                     transitions[src].append((r1, r2, nxt, w1, w2, mv1, mv2))
-
+    #Load the state data and transitions into the state object 
     states = [state(name, transitions.get(name, [])) for name in states_list]
 
     return states, start_state, accepting_state
 
 def main():
+    #Hard code in the file data structure 
     filename = "data_structure.txt"
+    #Main loop
     while(True):
+        #Ask the user for initial input 
         tape_input = input("Enter initial tape1 input string or (q)uit: ").strip()
+        #Exit condition 
         if tape_input == 'q':
             break
-
+        #Get the data from the file 
         states, start_state, accepting_state = load_tm(filename)
+        #Initalize the class object 
         tm = Turing(start_state, states, tape_input)
+        #Set the accepting state for the machine 
         tm.accepting = accepting_state
-
+        #Run the machine on the input given
         result = tm.simulate()
-
+        #Case for if the machine accepted the input 
         if result[0] == "ACCEPT":
             print("\nACCEPTED")
             output_tape = ''
+            #Loop over the tape to print out a clean version 
+            #Otherwise, it would have a bunch of 0s from the blanks
             for i in range(len(result[1])):
                 if result[1][i] == '1':
                     output_tape += '1'
+            #Print out the outpyt from the second tape 
             print("Output on Tape2: ", output_tape)
             print("Path of transitions:")
             print("Current state | Read tape 1 | Write tape 1 | Read tape 2 | Write tape 2 | New state")
+            #Print out the path the input took through the machine 
             for p in result[2]:
                 print(f"       {p[0]}           {p[1]}              {p[2]}              {p[3]}             {p[4]}            {p[5]}")
+        #Otherwise, print that the input was rejected 
         else:
             print("\nREJECTED")
 
--- Simulator/turing.py
+++ Simulator/turing.py
@@ -1,27 +1,37 @@
+#This class pairs a state name with all the transitions from that state 
 class state:
     def __init__(self,name, transitions):
         self.name = name
         self.trans = transitions
-    
+#Class for the turing machine   
 class Turing:
+    #We take in the start state, states, and the inital input on tape 1
     def __init__(self, start, states, input):
+        #Track the current state 
         self.current_state = start
         self.states = states
         self.accepting = None
+        #Track the head positions on the tape 
         self.head1 = 2
         self.head2 = 2
+        #Pad the tapes with 0s to act as blanks 
         self.tape1 = ['0', '0'] + list(input) + ['0', '0']
         #Using 0 as a blank 
         self.tape2 = ['0'] * (len(input)+4)
         self.path = []
-    
+    #Method that performs a transition 
     def transition(self):
+        #For every state in the states list
         for st in self.states:
+            #Look for the current state in the list
             if st.name == self.current_state:
+                #Loop over every transitions that is valid for this state 
                 for tr in st.trans:
+                    #Get data from transition
                     read1, read2, next_state, write1, write2, mv1, mv2 = tr
 
-                    # Expand tape1 if needed
+                    #We need to dynamically expand the tapes as they grow 
+                    #This avoids list indexing errors 
                     if self.head1 < 0:
                         self.tape1.insert(0, '0')
                         self.head1 = 0
@@ -34,30 +44,41 @@ class Turing:
                         self.head2 = 0
                     elif self.head2 >= len(self.tape2):
                         self.tape2.append('0')
-
+                    #Check if the current input on the tape matchs the transition 
                     if self.tape1[self.head1] == read1 and self.tape2[self.head2] == read2:
-
+                        #If it does match, we can overwrite the data at this position on the tape
+                        #with the new write values from the transition 
                         self.tape1[self.head1] = write1
                         self.tape2[self.head2] = write2
-                        
+                        #Save the state we are exiting in a temp var
+                        #This is so it can be used for the path 
                         old_state = self.current_state
+                        #Update the current state 
                         self.current_state = next_state
-
+                        
+                        #Adjust the head positions depending on the transition
                         if mv1 == 'R': self.head1 += 1
                         elif mv1 == 'L': self.head1 -= 1
-
+                        #Head 2 position 
                         if mv2 == 'R': self.head2 += 1
                         elif mv2 == 'L': self.head2 -= 1
-
+                        #Add this new transition data to the path 
                         self.path.append((old_state, read1, write1, read2, write2, next_state))
+                        #We return F to keep going 
                         return "F"
+        #If there is no valid transition for the input 
+        #We can halt the machine 
         return "HALT"
-
+    #This method simulates the machine by repeatedly running the transition method
+    #And checking for acceptance 
     def simulate(self):
+        #Loop until the machine halts 
         while True:
+            #If the machine hits an accepting state, we can terminate
             if self.current_state == self.accepting:
                 return ("ACCEPT", self.tape2, self.path)
-
+            #Check for transitions 
             result = self.transition()
+            #If we run out of transitions to take, we can halt and reject input
             if result == "HALT":
                 return ("REJECT", None)
